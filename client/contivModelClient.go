18548f2f87275334425d55fc92b647e1cda31794
jojimt <joji@cisco.com>
2016-09-02 03:46:00 -0700

    Track matching endpoint group from rule

--- client/contivModelClient.go
+++ client/contivModelClient.go
@@ -235,8 +235,8 @@ type EndpointGroup struct {
 
 type EndpointGroupLinkSets struct {
 	ExtContractsGrps map[string]Link `json:"ExtContractsGrps,omitempty"`
+	MatchRules       map[string]Link `json:"MatchRules,omitempty"`
 	Policies         map[string]Link `json:"Policies,omitempty"`
-	Rules            map[string]Link `json:"Rules,omitempty"`
 	Services         map[string]Link `json:"Services,omitempty"`
 }
 
@@ -429,10 +429,15 @@ type Rule struct {
 
 	// add link-sets and links
 	LinkSets RuleLinkSets `json:"link-sets,omitempty"`
+	Links    RuleLinks    `json:"links,omitempty"`
 }
 
 type RuleLinkSets struct {
 	Policies map[string]Link `json:"Policies,omitempty"`
+}
+
+type RuleLinks struct {
+	MatchEndpointGroup Link `json:"MatchEndpointGroup,omitempty"`
 }
 
 type RuleInspect struct {
